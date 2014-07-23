92b4613bcd15eb827212301bc8cdc2da568f5c85
Daniel Callander <0906578c@student.gla.ac.uk>
2014-07-23 12:33:46 +0100

    fixed tab/space mix

--- src/glasgow_baxter_helpers/setup.py
+++ src/glasgow_baxter_helpers/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-	packages=['glasgow_baxter_helpers'],
-	package_dir={'': 'src/'}
+    packages=['glasgow_baxter_helpers'],
+    package_dir={'': 'src/'}
 )
 
 setup(**d)
